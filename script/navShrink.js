34488f2f6fdccd304212bd0e7dee386adb3b6e54
AndreasTheScripter <AndreasTheScripter@Gmail.Com>
2016-12-18 16:36:22 +0700

    Delete navShrink.js

--- script/navShrink.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$(window).scroll(function() {
-		if($(document).scrollTop() > 1) {
-			$('#navbar').addClass('shrink');
-		} else {
-			$('#navbar').removeClass('shrink');
-		}
-	});
-});
